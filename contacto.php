43eb754e03be8d02d38fd7a87be31f2bcd5b8292
martinYulo15 <118919716+martinYulo15@users.noreply.github.com>
2022-11-23 10:10:41 -0600

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,3 @@
+<?php
+ echo"Este es el archivo de contacto creado en github, un saludazo desde Cuernavaca la frondosa capital del estado de Morelos"
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+  echo"este es un archivo index creado en github, un saludazo desde Durano capital"
+?>
