ebcd7fd3be462f0758797f3052fb39fdec337b44
martinYulo15 <118919716+martinYulo15@users.noreply.github.com>
2022-11-23 10:11:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-  echo"este es un archivo index creado en github, un saludazo desde Durano capital"
+  echo"este es un archivo index creado en github, un saludazo desde Durano capital";
+  echo"Hola mi amor este es otro mensaje desde github"
 ?>
