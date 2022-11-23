11feb724e399419c4cd3704a9a83a6c60a67c652
martinYulo15 <118919716+martinYulo15@users.noreply.github.com>
2022-11-23 10:11:51 -0600

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
   echo"este es un archivo index creado en github, un saludazo desde Durano capital";
-  echo"Hola mi amor este es otro mensaje desde github"
+  echo"Hola mi amor este es otro mensaje desde github";
 ?>
