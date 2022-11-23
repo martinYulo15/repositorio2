874a151e9f15bd361c081e32b3e143ef029d37e1
martinYulo15 <118919716+martinYulo15@users.noreply.github.com>
2022-11-23 10:12:56 -0600

    contacto.php

--- contacto.php
+++ contacto.php
@@ -1,3 +1,4 @@
 <?php
- echo"Este es el archivo de contacto creado en github, un saludazo desde Cuernavaca la frondosa capital del estado de Morelos"
+ echo"Este es el archivo de contacto creado en github, un saludazo desde Cuernavaca la frondosa capital del estado de Morelos";
+ echo"hola"
 ?>
